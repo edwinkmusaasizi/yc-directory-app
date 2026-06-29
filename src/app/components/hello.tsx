28f11464f609034716e06fab73bbc225d65bf9b0
Musaasizi Edwin Kyeyune <edwinkmusaasizi@gmail.com>
2026-06-29 17:50:48 +0300

    use client

---
+++ src/app/components/hello.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+function Hello(){
+    console.log("i am a Client component");
+    return (
+        <div>
+            <h1>Hello</h1>
+        </div>
+    );
+
+}
+
+export default Hello;
