db75634844a87a2585a731ab73cf189c82d24a4a
Musaasizi Edwin Kyeyune <edwinkmusaasizi@gmail.com>
2026-06-30 18:37:53 +0300

    authentication

--- yc_directory/app/(root)/layout.tsx
+++ yc_directory/app/(root)/layout.tsx
@@ -1,4 +1,4 @@
-import Navbar from "@/app/api/auth/[...nextauth]/Navbar";
+import Navbar from "@/app/components/Navbar";
 
 export default function Layout({children} : Readonly<{children: React.ReactNode}> ) {
     return (
