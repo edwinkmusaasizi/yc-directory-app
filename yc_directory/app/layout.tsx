987c74a6a9ed7d4aaed9d5bcb0a142a34265b8da
Musaasizi Edwin Kyeyune <edwinkmusaasizi@gmail.com>
2026-07-01 14:24:39 +0300

    Hero Section Complete

--- yc_directory/app/layout.tsx
+++ yc_directory/app/layout.tsx
@@ -1,6 +1,10 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import { Inter } from "next/font/google";
+import { cn } from "@/lib/utils";
+
+const inter = Inter({subsets:['latin'],variable:'--font-sans'});
 
 const workSans = localFont({
   src: [
@@ -56,7 +60,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={cn("font-sans", inter.variable)}>
       <body
           className={`${workSans.variable} font-work-sans antialiased`}
     >
